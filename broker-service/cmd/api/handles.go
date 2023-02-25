baeec6f146bdd815b9a2675526a85f761c886d70
Favour Olukayode <nerosilva522@gmail.com>
2023-02-25 15:39:23 +0100

    fix: fixing JSON syntaxt error

--- broker-service/cmd/api/handles.go
+++ broker-service/cmd/api/handles.go
@@ -8,7 +8,7 @@ import (
 type jsonResponse struct {
 	Error   bool   `json:"error"`
 	Message string `json:"message"`
-	Data    any    `json:"data"`
+	Data    any    `json:"data,omitempty"`
 }
 
 // We're using a raw http handler, my chest.
@@ -21,7 +21,7 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	out, _ := json.MarshalIndent(payload, "", "\t")
 
 	//Setting the header of response to accept json and the response code to 202
-	w.Header().Set("content-type", "application/json")
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusAccepted)
 
 	//Actually sending out the payload
