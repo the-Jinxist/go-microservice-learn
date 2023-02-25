5a2a403b1236b0d59b586d79b6fded984d1eea8c
Favour Olukayode <nerosilva522@gmail.com>
2023-02-25 17:57:16 +0100

    feat: we created helper functions to manageJson

--- broker-service/cmd/api/handles.go
+++ broker-service/cmd/api/handles.go
@@ -1,15 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
-
-type jsonResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Data    any    `json:"data,omitempty"`
-}
 
 // We're using a raw http handler, my chest.
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
@@ -18,12 +11,5 @@ func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 		Message: "Hit the broker!",
 	}
 
-	out, _ := json.MarshalIndent(payload, "", "\t")
-
-	//Setting the header of response to accept json and the response code to 202
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-
-	//Actually sending out the payload
-	w.Write(out)
+	_ = app.writeJson(w, http.StatusOK, payload)
 }
