f1e8ed7ff728c8c1610b93562af8f012608868a1
Favour Olukayode <nerosilva522@gmail.com>
2023-02-26 22:59:20 +0100

    feat: wrote up stub for authentication service

---
+++ authentication-service/cmd/api/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"authentication/data"
+	"database/sql"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "80"
+
+type Config struct {
+	DB     *sql.DB
+	Models data.Models
+}
+
+func main() {
+	log.Println("starting authentication service")
+
+	//Connect to database
+
+	//Setup config
+	app := Config{}
+
+	server := http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("error while starting service: %s", err)
+	}
+}
---
+++ authentication-service/go.mod
@@ -0,0 +1,11 @@
+module authentication
+
+go 1.19
+
+require golang.org/x/crypto v0.6.0
+
+require (
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/go-chi/chi/v5 v5.0.8 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
+)
