7fc3c3d78aa4e557d54f2c3a4cc2e7650b2ab381
Favour Olukayode <nerosilva522@gmail.com>
2023-03-12 11:49:49 +0100

    fix: added emitter to push event and tested

--- broker-service/cmd/api/handlers.go
+++ broker-service/cmd/api/handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"broker-service/event"
 	"bytes"
 	"encoding/json"
 	"errors"
@@ -59,7 +60,7 @@ func (app *Config) HandleSubmission(w http.ResponseWriter, r *http.Request) {
 		app.authenticate(w, requestPayload.Auth)
 		return
 	case "log":
-		app.logItem(w, requestPayload.Log)
+		app.logEventViaRabbit(w, requestPayload.Log)
 		return
 	case "mail":
 		app.sendMail(w, requestPayload.Mail)
@@ -200,4 +201,38 @@ func (app *Config) sendMail(w http.ResponseWriter, m MailPayload) {
 	payload.Message = "Message sent to: " + m.To
 
 	app.writeJson(w, http.StatusAccepted, payload)
+}
+
+func (app *Config) logEventViaRabbit(w http.ResponseWriter, l LogPayload) {
+	err := app.pushToQueue(l.Name, l.Data)
+	if err != nil {
+		app.errorJson(w, err)
+		return
+	}
+
+	var payload jsonResponse
+	payload.Error = false
+	payload.Message = "Logged via rabbit"
+
+	app.writeJson(w, http.StatusAccepted, payload)
+}
+
+func (app *Config) pushToQueue(name, msg string) error {
+	emitter, err := event.NewEventEmitter(app.Rabbit)
+	if err != nil {
+		return err
+	}
+
+	payload := LogPayload{
+		Name: name,
+		Data: msg,
+	}
+
+	json, _ := json.MarshalIndent(&payload, "", "\t")
+	err = emitter.Push(string(json), "log.INFO")
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
